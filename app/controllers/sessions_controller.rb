ca24e7aade7fe01e161a142dedeca0431c38ac45
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-03-08 17:42:58 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # binding.pry
+    if params[:name] == nil || params[:name] == ""
+      redirect_to '/sessions/new'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to login_path
+  end
+
+end
